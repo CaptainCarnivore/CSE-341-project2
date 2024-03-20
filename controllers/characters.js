f27b75bc3416d90f7571a459fd6b45f33d642729
CaptainCarnivore <ausjensen0451@byui.edu>
2024-03-20 16:09:21 -0700

    maybe fixed??

--- controllers/characters.js
+++ controllers/characters.js
@@ -20,12 +20,11 @@ const getSingle = async (req, res) => {
       }
 
     const characterId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('characters').find({ _id: characterId});
+    const result = await mongodb.getDatabase().db().collection('characters').find({ _id: characterId}).catch(error => console.error('error', error));;
     result.toArray().then((characters) => {
         if (characters.length == 0) {
             res.status(404).json(`Resource not found with id ${characterId}`)
         }
-        res.setHeader('Content-Type', 'application/json');
         res.status(200).json(characters[0]);
     });
 }; 
